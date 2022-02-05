82bc484e5c94e07eb3d718addd819019835aac83
kh9sd <kevin9sd@gmail.com>
2022-02-04 21:56:14 -0500

    whoooooooo more tests

--- tests lol/test_javadata.py
+++ tests lol/test_javadata.py
@@ -1,8 +1,50 @@
 import unittest
+from JavaData import JavaClass, Var, Method
+
 
 class MyTestCase(unittest.TestCase):
+    t_double = JavaClass("double", [], [])
+    t_int = JavaClass("int", [], [])
+    t_String = JavaClass("String", [], [])
+    t_boolean = JavaClass("boolean", [], [])
+
+    f_sum_diff = Method("sumDiff", t_double, [])
+    f_sum_motifs = Method("sumMotifs", t_int, [])
+    f_desc_list = Method("descList", t_String, [t_boolean])
+
+    f_total_diff = Method("totalDiff", t_double, [])
+    f_num_motifs = Method("numMotifs", t_int, [])
+    f_motif_info = Method("motifInfo", t_String, [])
+
+    t_imotif = JavaClass("IMotif", [], [f_total_diff,
+                                        f_num_motifs,
+                                        f_motif_info])
+    t_ilomotif = JavaClass("ILoMotif", [], [f_sum_diff,
+                                            f_sum_motifs,
+                                            f_desc_list])
+
+    t_conslomotif = JavaClass("ConsLoMotif", [Var("first", t_imotif),
+                                              Var("rest", t_ilomotif)]
+                              , [f_sum_diff,
+                                 f_sum_motifs,
+                                 f_desc_list])
+
     def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+        self.assertEqual(self.t_conslomotif.get_class_template(), ['Fields:',
+                                                                   'this.first ... IMotif',
+                                                                   'this.rest ... ILoMotif',
+                                                                   'Methods:',
+                                                                   'this.sumDiff() ... double',
+                                                                   'this.sumMotifs() ... int',
+                                                                   'this.descList(boolean) ... String',
+                                                                   'Methods for fields:',
+                                                                   'this.first.totalDiff() ... double',
+                                                                   'this.first.numMotifs() ... int',
+                                                                   'this.first.motifInfo() ... String',
+                                                                   'this.rest.sumDiff() ... double',
+                                                                   'this.rest.sumMotifs() ... int',
+                                                                   'this.rest.descList(boolean) ... String'])
+
 
 if __name__ == '__main__':
     unittest.main()
--- tests lol/test_main_methods.py
+++ tests lol/test_main_methods.py
@@ -1,8 +1,25 @@
+from Main import *
 import unittest
 
+
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_is_field(self):
+        self.assertEqual(is_field("asdasdsad.asdasd()"), False)
+        self.assertEqual(is_field("ass.bruh"), True)
+
+    def test_is_constructor(self):
+        self.assertEqual(is_constructor("Bruh.Bruh()"), True)
+        self.assertEqual(is_constructor("Bruh.Bruhh()"), False)
+        self.assertEqual(is_constructor("Bruh.Bruh(boolean, IMotif)"), True)
+
+    def test_str_to_method(self):
+        self.assertEqual(process_str_to_method("ILoMotif.descList(boolean)"),
+                         ("descList", "boolean"))
+        self.assertEqual(process_str_to_method("ConsLoMotif.sumMotifs()"),
+                         ("sumMotifs", ""))
+        self.assertEqual(process_str_to_method("ConsLoMotif.sumMotifs(amongus, int, sus)"),
+                         ("sumMotifs", "amongus, int, sus"))
+
 
 if __name__ == '__main__':
     unittest.main()
