62ffc6b913237abd0c5fa8c5100e1bcbc9669290
Kevin Huang <41244296+kh9sd@users.noreply.github.com>
2022-02-12 18:08:28 -0500

    supporting inheritance

--- JavaData.py
+++ JavaData.py
@@ -1,69 +1,171 @@
+from OrderedSet import OrderedSet
+
+
 class JavaClass:
-    def __init__(self, name, fields=None, methods=None):
-        self.name = name  # string
+    def __init__(self, name, fields=None, methods=None, ancestor=None):
+        """
+        Constructor for JavaClass
+
+        self.name : String
+        self.fields : List of Var
+        self.methods : List of Method
+        self.ancestor : JavaClass
+        """
+        self.name = name
+
         if fields:
-            self.fields = fields  # Vars
+            self.fields = fields
         else:
             self.fields = []
 
         if methods:
             self.methods = methods
         else:
-            self.methods = []  # Methods
+            self.methods = []
+
+        self.ancestor = ancestor
 
     def get_class_template(self):
+        """
+        Returns list of Strings, each being a line in the class template
+        """
         result = ["Fields:"]
-        result.extend(("this." + field.template_entry()) for field in self.fields)
+        result.extend(("this." + field) for field in self.get_fields_temps())
 
         result.append("Methods:")
         result.extend(("this." + method) for method in self.get_method_temps())
 
         result.append("Methods for fields:")
-
-        for field in self.fields:
+        for field in self.get_all_fields():
             result.extend(("this." + field_method) for field_method in field.get_method_list())
 
         return result
 
-    def custom_var_temp(self, var_name):
-        return [(var_name+"."+method) for method in self.get_method_temps()]
+    def get_all_fields(self):
+        """
+        Returns a set of all field Vars and inherited fields from ancestors.
+
+        Ordered by insertion
+        """
+        if self.ancestor is None:
+            var_set = OrderedSet()
+        else:
+            var_set = self.ancestor.get_all_fields()
+
+        for field in self.fields:
+            var_set.add(field)
+
+        return var_set
+
+    def get_all_methods(self):
+        """
+        Returns a set of all Methods and inherited Methods from ancestors.
+
+        Ordered by insertion
+        """
+        if self.ancestor is None:
+            method_set = OrderedSet()
+        else:
+            method_set = self.ancestor.get_all_methods()
+
+        for method in self.methods:
+            method_set.add(method)
+
+        return method_set
+
+    def get_fields_temps(self):
+        """
+        Returns list of Strings, each being a single template entry for a class field
+        """
+        return [field.template_entry_str() for field in self.get_all_fields()]
 
     def get_method_temps(self):
-        return (method.template_entry() for method in self.methods)
+        """
+        Returns list of Strings, each being a single template entry for a class method
+        """
+        return [method.template_entry_str() for method in self.get_all_methods()]
 
     def add_field(self, field):
+        """
+        Setter method for adding a field
+
+        Parameters:
+            field: Var
+        """
         self.fields.append(field)
 
     def add_method(self, method):
+        """
+        Setter method for adding a method
+
+        Parameters:
+            method: Method
+        """
         self.methods.append(method)
+
+    def set_ancestor(self, anc):
+        """
+        Setter method for setting ancestor for class
+
+        Parameters:
+            anc: JavaClass
+        """
+        self.ancestor = anc
 
     def __str__(self):
         return self.name
+
+    def __eq__(self, other):
+        return str(self) == str(other)
 
 
 class Var:
-    def __init__(self, name, type):
+    def __init__(self, name, t):
         self.name = name  # String
-        self.type = type  # JavaClass
+        self.type = t  # JavaClass
 
-    def template_entry(self):
+    def template_entry_str(self):
+        """
+        Returns String representation of Var in template format
+        """
         return self.name + " ... " + str(self.type)
 
     def get_method_list(self):
-        return ((self.name + "." + method_temp_line) for method_temp_line in self.type.get_method_temps())
+        """
+        Returns generator of Strings, each being a representation of a method callable on this Var
+        """
+        return ((self.name + "." + method_temp) for method_temp in self.type.get_method_temps())
 
     def __str__(self):
         return self.name
+
+    def __eq__(self, other):
+        return self.name == other.name and self.type == other.type
+
+    def __hash__(self):
+        return hash(str(self))
 
 
 class Method:
     def __init__(self, name, r_type, paras):
         self.name = name  # String
         self.return_type = r_type  # JavaClass
-        self.parameters = paras  # JavaClasses
+        self.parameters = paras  # list of JavaClass
 
-    def template_entry(self):
+    def template_entry_str(self):
+        """
+        Returns String representation of Method in template format
+        """
         return str(self) + " ... " + str(self.return_type)
 
     def __str__(self):
         return self.name + "(" + ", ".join(str(para) for para in self.parameters) + ")"
+
+    def __eq__(self, other):
+        return self.name == other.name and \
+               self.return_type == other.return_type and \
+               len(self.parameters) == len(other.parameters) and \
+               all(this_para == other_para for this_para, other_para in zip(self.parameters, other.parameters))
+
+    def __hash__(self):
+        return hash(str(self))
--- Main.py
+++ Main.py
@@ -11,12 +11,21 @@ class Category(Enum):
 
 
 def get_trimmed_name(name, class_name):
+    """
+    Gets trimmed name like "IMotif.first" and class name "IMotif"
+    and returns "first"
+
+    :param name: String
+    :param class_name: String
+    :return: String
+    """
     return name[len(class_name) + 1:]
 
 
-def process_str_to_method(s):
+def process_method_str(s):
     """
-    [desc]
+    Gets input like bruh(Boolean, IMotif)
+    and outputs "bruh", "Boolean, IMotif"
 
     :param s: String
     :return: 2-tuple of String, String
@@ -27,30 +36,44 @@ def process_str_to_method(s):
 
 
 def find_category(input_str):
-    match len(input_str.split("(")):
-        case 1:  # either class name or field
-            name = input_str
-            match len(name.split()):
-                case 1:
-                    return Category.CLASS
-                case 2:
-                    return Category.FIELD
+    """
+    Gets raw line from input file and determines whether it is
+    a class, field, constructor, or method
 
-        case 2:  # method, have to ignore constructors
-            name, _ = input_str.split("(")
-            class_name, method_name = name.split(".")
+    :param input_str: String
+    :return: enumeration of Category
+    """
+    par_splits = input_str.split("(")
+    if len(par_splits) == 1:  # either class name or field
+        if "." not in input_str:
+            return Category.CLASS
+        else:
+            return Category.FIELD
 
-            if class_name == method_name:
-                return Category.CONSTRUCTOR
-            return Category.METHOD
+    elif len(par_splits) == 2:  # method, have to ignore constructors
+        name, _ = par_splits
+        class_name, method_name = name.split(".")
+
+        if class_name == method_name:
+            return Category.CONSTRUCTOR
+        return Category.METHOD
 
     raise(ValueError("Line '{input_str}' is not a valid input"))
 
 
 def parse_input_file(location):
-    def add_if_not_in_dict(j_type):
+    """
+    Reads text file and produces finished dict containing
+    all JavaClasses with inner structures
+
+    :param location: String
+    :return: dict(String, JavaClass)
+    """
+    def get_from_dict_always(j_type):
         """
-        [desc]
+        Returns JavaClass object corresponding to string in str_class_dict
+
+        SIDE EFFECT: If not in dict, adds to dict keyed by class name
 
         :param j_type:
         :return: JavaClass
@@ -64,63 +87,76 @@ def parse_input_file(location):
 
     def create_var(ident, j_type):
         """
-        [desc]
+        creates Var object holding name and type JavaClass
 
         :param ident: String
         :param j_type: String
         :return: Var
         """
-        return Var(ident, add_if_not_in_dict(j_type))
+        return Var(ident, get_from_dict_always(j_type))
 
     def create_para_list(paras):
         """
-        [desc]
+        creates list of JavaClass from inputted String
 
         :param paras: String, formatted like "boolean, IMotif, int"
         :return: list of JavaClass
         """
-        return [add_if_not_in_dict(para.strip()) for para in paras.split(",")]
+        return [get_from_dict_always(para.strip()) for para in paras.split(",")]
 
     def create_method(ident, j_type):
         """
-        [desc]
+        Creates method with name, return type as JavaClass, and list of parameters as
+        list of JavaClass
 
         :param ident: String
         :param j_type: String
         :return: Method
         """
-        name, paras = process_str_to_method(ident)
-        return Method(name, add_if_not_in_dict(j_type), create_para_list(paras))
+        name, paras = process_method_str(ident)
+        return Method(name, get_from_dict_always(j_type), create_para_list(paras))
 
     str_class_dict = {}
 
     with open(location, "r") as f:
         for line in f:
-            match find_category(line):
-                case Category.CLASS:
-                    cur_class_name = line.strip()
-                    cur_class = add_if_not_in_dict(cur_class_name)
+            line = line.strip()
+            cat = find_category(line)
 
-                    print("On new class:", cur_class_name)
+            if cat == Category.CLASS:
+                cur_class_name, *rest = line.split()
+                cur_class = get_from_dict_always(cur_class_name)
 
-                case Category.FIELD:
-                    identifier, j_class = line.split()
-                    identifier = get_trimmed_name(identifier, cur_class_name)
+                if rest:
+                    cur_class.set_ancestor(get_from_dict_always(rest[0]))
 
-                    cur_class.add_field(create_var(identifier, j_class))
+                print("On new class:", cur_class_name)
 
-                case Category.METHOD:
-                    identifier, j_class = line.rsplit(sep=None, maxsplit=1)
-                    identifier = get_trimmed_name(identifier, cur_class_name)
+            elif cat == Category.FIELD:
+                identifier, j_class = line.split()
+                identifier = get_trimmed_name(identifier, cur_class_name)
 
-                    cur_class.add_method(create_method(identifier, j_class))
-                case Category.CONSTRUCTOR:
-                    pass
+                cur_class.add_field(create_var(identifier, j_class))
+
+            elif cat == Category.METHOD:
+                identifier, j_class = line.rsplit(sep=None, maxsplit=1)
+                identifier = get_trimmed_name(identifier, cur_class_name)
+
+                cur_class.add_method(create_method(identifier, j_class))
+
+            elif cat == Category.CONSTRUCTOR:
+                pass
 
     return str_class_dict
 
 
 def get_formatted_temp(temp_list):
+    """
+    Joins together list into String by new lines
+
+    :param temp_list: Sequence
+    :return: String
+    """
     return "\n".join(temp_list)
 
 
@@ -129,10 +165,11 @@ if __name__ == "__main__":
 
     print(f"Now reading data from {INPUT_FILE_LOC}")
     name_to_class_map = parse_input_file(INPUT_FILE_LOC)
-    print("Done reading!")
+    print("Done reading, all detected classes (including builtins) are:")
 
-    # print(name_to_class_map)
-    # print(get_formatted_template(name_to_class_map["ILoMotif"]))
+    for c_name in name_to_class_map:
+        print(c_name)
+
     while True:
         choice = input("\nEnter 'c' to get a template for a class, "
                        "enter 'p' to get a template for a method parameter\n").lower()
@@ -146,16 +183,16 @@ if __name__ == "__main__":
                 print("Invalid class name, returning to start\n")
 
         elif choice == "p":
-            para_class = input("\nWhat class is your variable?\n").strip()
+            input_class = input("\nWhat class is your variable?\n").strip()
 
-            if para_class in name_to_class_map:
+            if input_class in name_to_class_map:
+                var_class = name_to_class_map[input_class]
                 var_name = input("\nWhat is the name of your variable?\n")
-                print(get_formatted_temp(name_to_class_map[para_class].custom_var_temp(var_name)))
+
+                new_var = Var(var_name, var_class)
+                print(get_formatted_temp(new_var.get_method_list()))
             else:
                 print("Invalid class name, returning to start\n")
 
         else:
             print("\nInvalid option, try again")
-
-
-    # print(get_formatted_template(name_to_class_map["ConsLoMotif"]))
